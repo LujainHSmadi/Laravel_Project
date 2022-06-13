d3ff79273a3a71e660868dc5bbb26a0eb01651e4
LujainHSmadi <100359593+LujainHSmadi@users.noreply.github.com>
2022-06-14 00:55:09 +0300

    main

--- Helper/resources/views/admin/donate/donateEdit.blade.php
+++ Helper/resources/views/admin/donate/donateEdit.blade.php
@@ -22,29 +22,33 @@
 
            <div class="form-group">
             <label for="input-1">Name</label>
-            <input type="text" class="form-control" id="input-1" placeholder="Enter Your Name" name="name">
+            <input type="text" class="form-control" id="input-1" placeholder="Enter Your Name" name="name" value="{{$donate->name}}">
            </div>
            <div class="form-group">
                <label for="input-2">Email</label>
-               <input type="email" class="form-control" name="email" id="input-2" placeholder="Enter Your Email Address">
+               <input type="email" class="form-control" name="email" id="input-2" placeholder="Enter Your Email Address" {{$donate->email}}>
             </div>
             <div class="form-group">
                 <label for="input-4">Number</label>
-                <input type="phone" class="form-control" name="number" id="input-4" placeholder="Enter Password">
+                <input type="phone" class="form-control" name="number" id="input-4" placeholder="Enter Password" {{$donate->Password}}>
             </div>
             <div class="form-group">
              <label for="input-1">Address</label>
-             <input type="text" class="form-control" id="input-1" placeholder="Enter Your Address" name="address">
+             <input type="text" class="form-control" id="input-1" placeholder="Enter Your Address" name="address" {{$donate->address}}>
             </div>
             <div class="form-group">
              <label for="input-1">Tools</label>
              {{-- <input type="text" class="form-control" id="input-1" placeholder="Tools" name="tools"> --}}
  <div class="form-group">
                                 <label><strong>Tools :</strong></label><br>
-                                <label><input type="checkbox" name="tools[]" value="Red"> Red</label>
-                                <label><input type="checkbox" name="tools[]" value="Blue"> Blue</label>
-                                <label><input type="checkbox" name="tools[]" value="Green"> Green</label>
-                                <label><input type="checkbox" name="tools[]" value="Yellow"> Yellow</label>
+                                <label><input type="checkbox" name="tools[]" value="paint"> paint</label> <br> 
+                         <label><input type="checkbox" name="tools[]" value="Bricks"> Bricks</label><br>
+                         <label><input type="checkbox" name="tools[]" value="Grit"> Grit</label><br>
+                         <label><input type="checkbox" name="tools[]" value="Rebar"> Rebar</label>
+                         <label><input type="checkbox" name="tools[]" value="Cement"> Cement</label>
+                         <label><input type="checkbox" name="tools[]" value="Electrical wires"> Electrical wires</label>
+                         <label><input type="checkbox" name="tools[]" value="Doors">Doors</label>
+                         <label><input type="checkbox" name="tools[]" value="Windows">Windows</label>
                             </div>  
              
             </div>
